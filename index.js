c0c99be7d722c26ee9349f38415c69c8f5530cc2
Henry <henry@kirkness.com>
2018-12-05 22:12:24 +0000

    Added inc option

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const semver = require('semver');
 
 const ARGS = {
 	FILE_PATH: '--file',
-	PROP_PATH: '--key'
+	PROP_PATH: '--key',
+	PATCH: '--patch',
+	PRERELEASE: '--prerelease',
+	MINOR: '--minor',
+	MAJOR: '--major'
 };
 
 const getFilePath = (file) => path.resolve(process.cwd(), file);
@@ -17,7 +21,7 @@ const setFile = (file, json) => fs.writeFileSync(getFilePath(file), JSON.stringi
 const getArg = (argName) => {
 	const arg = process.argv.find((arg) => arg.startsWith(argName));
 	if (!arg) return;
-	return arg.replace(`${argName}=`, '');
+	return arg.replace(`${argName}=`, '') || true;
 };
 
 const versionFile = getArg(ARGS.FILE_PATH) || './package.json';
@@ -26,17 +30,45 @@ const versionPath = getArg(ARGS.PROP_PATH) || 'version';
 
 const config = getFile(versionFile);
 const currentVersion = get(config, versionPath);
+const setNextFile = (nextVersion) => {
+	setFile(versionFile, set(config, versionPath, nextVersion));
+	console.log();
+	console.log(`New version: ${nextVersion}`);
+	console.log();
+};
 
 if (!currentVersion) throw new Error('Current version not set.');
 
-const prerelease = semver.inc(currentVersion, 'prerelease', 'rc');
-const patch = semver.inc(currentVersion, 'patch');
-const minor = semver.inc(currentVersion, 'minor');
-const major = semver.inc(currentVersion, 'major');
+const versions = {
+	prerelease: semver.inc(currentVersion, 'prerelease', 'rc'),
+	patch: semver.inc(currentVersion, 'patch'),
+	minor: semver.inc(currentVersion, 'minor'),
+	major: semver.inc(currentVersion, 'major')
+};
 
 console.log();
 console.log(`Current version: ${currentVersion}`);
 console.log();
+
+if (getArg(ARGS.PRERELEASE)) {
+	setNextFile(versions.prerelease);
+	process.exit(0);
+}
+
+if (getArg(ARGS.PATCH)) {
+	setNextFile(versions.patch);
+	process.exit(0);
+}
+
+if (getArg(ARGS.MINOR)) {
+	setNextFile(versions.minor);
+	process.exit(0);
+}
+
+if (getArg(ARGS.MAJOR)) {
+	setNextFile(versions.major);
+	process.exit(0);
+}
 
 inquirer
 	.prompt([
@@ -45,18 +77,17 @@ inquirer
 			name: 'nextVersion',
 			message: 'Update version to',
 			choices: [
-				{ name: `Prerelease:  ${prerelease}`, value: prerelease },
-				{ name: `Patch:       ${patch}`, value: patch },
-				{ name: `Minor:       ${minor}`, value: minor },
-				{ name: `Major:       ${major}`, value: major },
+				{ name: `Prerelease:  ${versions.prerelease}`, value: versions.prerelease },
+				{ name: `Patch:       ${versions.patch}`, value: versions.patch },
+				{ name: `Minor:       ${versions.minor}`, value: versions.minor },
+				{ name: `Major:       ${versions.major}`, value: versions.major },
 				{ name: `Don't update.`, value: null }
 			]
 		}
 	])
 	.then((answer) => {
 		if (!answer.nextVersion) return process.exit(0);
-		const newConfigFile = set(config, versionPath, answer.nextVersion);
-		setFile(versionFile || './package.json', newConfigFile);
+		setNextFile(answer.nextVersion);
 		process.exit(0);
 	})
 	.catch((e) => {
