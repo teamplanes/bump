3f3822aecc9e67d1af31d91206a7fa8386629593
Henry <henry@kirkness.com>
2018-12-05 21:18:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# bump
+
+`bump` any semver version in any JSON file via a simple and basic CLI.
+
+### example
+
+Bump will by default bump `version` in `package.json`. 
+
+```sh
+bump
+
+Current version: 0.0.1-rc.0
+
+? Update version to (Use arrow keys)
+❯ Prerelease:  0.0.1-rc.1
+  Patch:       0.0.1
+  Minor:       0.1.0
+  Major:       1.0.0
+  Don't update.
+```
+
+But you can also pass in a `--file` and/or a `--key` arg.
+
+```sh
+bump --file=./app.json --key=expo.version
+```
+
+---
+
+Handy as commit hook with `husky`.
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const get = require('lodash.get');
+const set = require('lodash.set');
+const inquirer = require('inquirer');
+const semver = require('semver');
+
+const ARGS = {
+	FILE_PATH: '--file',
+	PROP_PATH: '--key'
+};
+
+const getFilePath = (file) => path.relative(__dirname, file);
+const getFile = (file) => JSON.parse(fs.readFileSync(getFilePath(file)));
+const setFile = (file, json) => fs.writeFileSync(getFilePath(file), JSON.stringify(json, null, 2), 'utf8');
+const getArg = (argName) => {
+	const arg = process.argv.find((arg) => arg.startsWith(argName));
+	if (!arg) return;
+	return arg.replace(`${argName}=`, '');
+};
+
+const versionFile = getArg(ARGS.FILE_PATH) || './package.json';
+// .version will be the default for package.json projects
+const versionPath = getArg(ARGS.PROP_PATH) || 'version';
+
+const config = getFile(versionFile);
+const currentVersion = get(config, versionPath);
+
+if (!currentVersion) throw new Error('Current version not set.');
+
+const prerelease = semver.inc(currentVersion, 'prerelease', 'rc');
+const patch = semver.inc(currentVersion, 'patch');
+const minor = semver.inc(currentVersion, 'minor');
+const major = semver.inc(currentVersion, 'major');
+
+console.log();
+console.log(`Current version: ${currentVersion}`);
+console.log();
+
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			name: 'nextVersion',
+			message: 'Update version to',
+			choices: [
+				{ name: `Prerelease:  ${prerelease}`, value: prerelease },
+				{ name: `Patch:       ${patch}`, value: patch },
+				{ name: `Minor:       ${minor}`, value: minor },
+				{ name: `Major:       ${major}`, value: major },
+				{ name: `Don't update.`, value: null }
+			]
+		}
+	])
+	.then((answer) => {
+		if (!answer.nextVersion) return process.exit(0);
+		const newConfigFile = set(config, versionPath, answer.nextVersion);
+		setFile(versionFile || './package.json', newConfigFile);
+		process.exit(0);
+	})
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	});
